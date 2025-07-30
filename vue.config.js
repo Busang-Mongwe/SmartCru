ae9718e6e67006120c8338c4794c54aec010266b
BusangMongwe <bmongwe@sts3d.co.za>
2025-07-30 14:21:05 +0200

    Adding repo name in config js

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,7 @@
-// If using Vite (most common for Vue 3)
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'SmartCru' // Replace with your actual repo name
+  base: '/SmartCru/'
 })
-
-// If using Vue CLI (Vue 2 or older Vue 3 projects)
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'SmartCru'
-    : '/'
-}
